22b7cce5fba91ce2f8d0c9419ff9e4c791c9c810
Roberto Navarro <li.roberto.navarro@gmail.com>
2023-09-15 21:02:44 -0600

    Integracion de Firebase y creacion del login

--- app/src/main/java/com/rnavarro/bzpaylogin/MainActivity.kt
+++ app/src/main/java/com/rnavarro/bzpaylogin/MainActivity.kt
@@ -2,10 +2,23 @@ package com.rnavarro.bzpaylogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val Usuario: String = ""
+    val Password:String = ""
+
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Registrar(view: View){
+        Toast.makeText(this, "Mando a la pagina de Registro", Toast.LENGTH_SHORT).show()
     }
 }
